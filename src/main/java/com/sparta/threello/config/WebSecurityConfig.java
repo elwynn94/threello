5e916d35c18ef6bca6fdeafe682776e055c3f637
Dongseon <elwynn94@gmail.com>
2024-07-11 20:19:51 +0900

    Fix : WebSecurityConfig 수정

--- src/main/java/com/sparta/threello/config/WebSecurityConfig.java
+++ src/main/java/com/sparta/threello/config/WebSecurityConfig.java
@@ -1,42 +1,83 @@
 package com.sparta.threello.config;
 
 
+import com.sparta.threello.jwt.JwtUtil;
+import com.sparta.threello.repository.UserRepository;
+import com.sparta.threello.security.JwtAuthenticationFilter;
+import com.sparta.threello.security.JwtAuthorizationFilter;
+import com.sparta.threello.security.UserDetailsServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
-@EnableWebSecurity
+@EnableWebSecurity // Spring Security 지원을 가능하게 함
+@RequiredArgsConstructor
 public class WebSecurityConfig {
 
+    private final JwtUtil jwtUtil;
+    private final UserDetailsServiceImpl userDetailsService;
+    private final AuthenticationConfiguration authenticationConfiguration;
+    private final UserRepository userRepository;
+
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration)
+            throws Exception {
+        return configuration.getAuthenticationManager();
+    }
 
-        http
-                .authorizeHttpRequests((auth) -> auth
-                    // 로그인, 메인 화면, 회원 가입 모든 사용자에게 허용
-                    .requestMatchers("/", "/users", "/users/login").permitAll()
-                    // 회원탈퇴
-                    .requestMatchers(HttpMethod.PATCH, "/users").authenticated()
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter() throws Exception {
+        JwtAuthenticationFilter filter = new JwtAuthenticationFilter(jwtUtil, userRepository);
+        filter.setAuthenticationManager(authenticationManager(authenticationConfiguration));
+        return filter;
+    }
 
+    @Bean
+    public JwtAuthorizationFilter jwtAuthorizationFilter() {
+        return new JwtAuthorizationFilter(jwtUtil, userDetailsService, userRepository);
+    }
 
-                );
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        // CSRF 설정
+        http.csrf(AbstractHttpConfigurer::disable);
 
-        http
-                .formLogin((auth)->auth
-                        // 권한이 필요한 API 요청 페이지 이전에 나타나야할 로그인페이지를 설정
-                        .loginPage("/users/login")
-                        // 프론트에서 넘겨줘야하는 데이터를 로그인처리해주는 api요청 설정
-                        .loginProcessingUrl("/loginProc")
-                        .permitAll()
-                );
+        http.sessionManagement((sessionManagement) ->
+                sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        );
 
-        //csrf 설정 disable시킴
-        http
-                .csrf((auth) -> auth.disable());
+        http.authorizeHttpRequests((authorizeHttpRequests) ->
+                authorizeHttpRequests
+                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations())
+                        .permitAll() // resources 접근 허용 설정
+                        .requestMatchers("/").permitAll() // 메인 페이지 요청 허가
+                        .requestMatchers("/error").permitAll()
+                        .requestMatchers("/users/signup", "/users/login").permitAll() //
+                        .requestMatchers("/email-verification/send", "/email-verification/resend",
+                                "/email-verification/verify").permitAll() // 이메일 인증 관련 요청 허가
+                        .requestMatchers(HttpMethod.GET, "/users/**").permitAll()
+                        .requestMatchers("/api/comment/**").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/users/**").authenticated()
+                        .requestMatchers(HttpMethod.PUT, "/users/**").authenticated()
+                        .requestMatchers(HttpMethod.PATCH, "/users/**").authenticated()
+                        .requestMatchers(HttpMethod.DELETE, "/users/**").authenticated()
+                        .anyRequest().authenticated() // 그 외 모든 요청 인증처리
+        );
+
+        // 필터 관리
+        http.addFilterBefore(jwtAuthorizationFilter(), JwtAuthenticationFilter.class);
+        http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
